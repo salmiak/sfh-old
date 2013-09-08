29a03e45bf4ab1301cdf846d9efbd811071afde5
Alfred Gunnarsson <alfred@salmiakmedia.se>
2013-09-08 10:53:39 +0200

    Started work on footer.

--- wp-content/themes/sfh/footer.php
+++ wp-content/themes/sfh/footer.php
@@ -1,3 +1,24 @@
+  <footer>
+    <div class="wrapper">
+      <div class="container_6">&nbsp;</div>
+      <div class="container_3">
+        <div class="inside">
+          <div class="map">Google karta</div>
+          <adress>
+            Skaftö Folkteshus<br/>
+            Alfågelstigen 2<br/>
+            451 79 Grundsund
+          </adress>
+          <p><strong>Tel</strong> 0705 321 995<br/>
+            <strong>Mail </strong> info@skaftofolketshus.se
+          </p>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+  </footer>
+
 	
 	<!-- Skript som uppdaterar media-query när man ändrar storlek på webbläsarfönster - http://code.google.com/p/css3-mediaqueries-js/ -->
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/css3-mediaqueries.js"></script>	  
